831c9c91fc1d7a1e29bba7468ab54db2c7135fd4
Jakub Nabaglo <j@nab.gl>
2021-04-11 19:02:14 +1000

    Cleanup

--- sgdist/sgdistimpl.cpp
+++ sgdist/sgdistimpl.cpp
@@ -1,53 +1,6 @@
 #include <cmath>
 #include <cstddef>
 
-template<typename T>
-T otdist(T const* pos1,
-         T const* mass1,
-         size_t n1,
-         T const* pos2,
-         T const* mass2,
-         size_t n2) {
-  T cumul = 0.0;
-  T mass_balance = 0.0;
-
-  size_t i1 = 0;
-  size_t i2 = 0;
-  T currpos1 = pos1[i1];
-  T currpos2 = pos1[i2];
-  while (true) {
-    T mass_moved;
-    if (mass_balance >= 0) {
-      if (i1 >= n1) break;
-      T new_mass = mass1[i1];
-      currpos1 = pos1[i1];
-      mass_moved = std::fmin(new_mass, mass_balance);
-      mass_balance -= new_mass;
-      ++i1;
-    } else {
-      if (i2 >= n2) break;
-      T new_mass = mass2[i2];
-      currpos2 = pos2[i2];
-      T mass_moved = std::fmin(new_mass, -mass_balance);
-      mass_balance += new_mass;
-      ++i2;
-    }
-    T diff = currpos1 - currpos2;
-    T diffsq = diff * diff;
-    cumul = std::fma(mass_moved, diffsq, cumul);
-  }
-
-  return std::sqrt(cumul);
-}
-
-template float otdist<float>(
-  float const* pos1, float const* mass1, size_t n1,
-  float const* pos2, float const* mass2, size_t n2);
-template double otdist<double>(
-  double const* pos1, double const* mass1, size_t n1,
-  double const* pos2, double const* mass2, size_t n2);
-
-
 
 template<typename T>
 T sgdist(T const* pos1,
@@ -92,6 +45,7 @@ T sgdist(T const* pos1,
 
   return std::sqrt(cumul_S);
 }
+
 
 template float sgdist<float>(
   float const* pos1, float const* mass1, size_t n1,
